6448664b258f2a9a7f4c543d9226f4064a699961
ivinnichek <vinnichekira@gmail.com>
2017-12-04 01:17:12 +0300

    DAL.EF

---
+++ DAL.EF/AccountOwner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class AccountOwner
+    {
+        public int AccountOwnerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ DAL.EF/AccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class AccountType
+    {
+        public int AccountTypeId { get; set; }
+        public string Type { get; set; }
+    }
+}
